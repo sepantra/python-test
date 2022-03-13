31e1a32149f76b837176ee0610e3ebb9a46b2dd3
Setareh Malekdoust <malekdoust.setareh@gmail.com>
2022-03-13 16:46:37 +0330

    220310-SMP-changes are done about read me file and codes

--- 1-homework/README.md
+++
@@ -1,5 +0,0 @@
-# python-first Project
-# python-first Project
-Dear user :
-you may have python programme in your pc,if not you may download it from The official python website,
-after that you can push the F5 bottom and run the program.
--- 1-homework/index.py
+++ 1-homework/index.py
@@ -1,13 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar  3 17:27:08 2022
-
-@author: Gv Shop
-"""
-
+import pprint
 Names=["Sayid","Moeen","Setareh"]
 Roles=["Project Manager","Web Developer","Python Programmer"]
 
-new={ "Hello "+Names[i]:"Role:" +Roles[i] for i in range(3)}
-import pprint
+new={ "Hello "+Names[i]:["Role:" +Roles[i]] for i in range(3)}
+
 pprint.pprint(new)
