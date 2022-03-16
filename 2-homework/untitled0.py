235e9e33403c7645a0256750c158ab5e995bbdbb
Sayid Malekdoost <sayid.malekdoost@gmail.com>
2022-03-16 18:20:50 +0100

    220316: minor change in messaging

--- 2-homework/untitled0.py
+++ 2-homework/untitled0.py
@@ -1,7 +1,7 @@
 array = ['\n']
 isLoop = True
 while isLoop :
-    words= input('Write any words to sort them in an alphabetical order,to see the results please write "cancel" : ')
+    words= input('Type any words to sort them in an alphabetical order,to see the results please write "cancel" : ')
     if words != 'cancel':
         array.append(words)
     elif words == 'cancel' : isLoop = False
