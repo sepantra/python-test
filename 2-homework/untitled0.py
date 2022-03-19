11ccf5d7f5cca5b073d17e138fcfb64222836303
Sayid Malekdoost <sayid.malekdoost@gmail.com>
2022-03-19 19:18:52 +0100

    220319: refactored var names

--- 2-homework/untitled0.py
+++ 2-homework/untitled0.py
@@ -1,8 +1,9 @@
-array = ['\n']
+outputWords = ['\n']
 isLoop = True
 while isLoop :
-    words= input('Type any words to sort them in an alphabetical order,to see the results please write "cancel" : ')
-    if words != 'cancel':
-        array.append(words)
-    elif words == 'cancel' : isLoop = False
-print(*sorted(array), sep = '\n')
+    inputWord = input('Type any words to sort them in an alphabetical order,to see the results please write "cancel" : ')
+    if inputWord != 'cancel':
+        outputWords.append(inputWord)
+    elif inputWord == 'cancel' : 
+        isLoop = False
+print(*sorted(outputWords), sep = '\n')
